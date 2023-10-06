242dcc30eaae4520ed78d7aa83ae2e3f352f0dd1
Replit user <>
2023-10-06 00:15:13 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -95,7 +95,13 @@ List* get_adj_nodes(Node* n){
       for(int num = 1 ; num <= 9; num++){
         Node* new_node = copy(n);
         new_node->sudo[filaVacia][columnaVacia] = num;
-        pushBack(list, new_node);
+
+
+        if(is_valid(new_node)){
+          pushBack(list,new_node);
+        }else{
+          free(new_node);
+        }
       }
     }
   

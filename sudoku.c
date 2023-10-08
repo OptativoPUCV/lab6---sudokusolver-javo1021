7181f1e802025b3cb78fff49cf6021c12195df35
Replit user <>
2023-10-08 02:11:58 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -66,7 +66,7 @@ int is_valid(Node* n){
         if(subMatrizAux[subMatriz_i][num] == 1){
           return 0;
         } else{
-          subMatrizAux[fila][num] = 1;
+          subMatrizAux[subMatriz_i][num] = 1;
         }
         
       }
@@ -157,7 +157,7 @@ Node* DFS(Node* initial, int* cont){
 
 
 
-
+/*
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -168,4 +168,4 @@ int main( int argc, char *argv[] ){
   print_node(final);
 
   return 0;
-}
+}*/
